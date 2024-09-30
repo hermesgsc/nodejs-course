af1f738255aa768d0ca5f880f37347c2267ec836
hermes gabriel <programadorgabriel22@gmail.com>
2024-09-30 10:03:05 -0300

    feat: writing with pure nodejs on html page

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const http = require('http')
 
 
 const server = http.createServer((req, res) => {
-  console.log(req, res)
+  console.log(req.url, req.method, req.headers)
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>')
+  res.write('<head><title>NodeJS Course</title></head>')
+  res.write('<body><p>NodeJS Course</p></body>')
+
+  res.write('</html>')
+  res.end()
 });
 
 server.listen(1910);

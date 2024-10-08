3b2baa0e1f9559245185bf4c4ab903575ebb8ba4
hermes gabriel <programadorgabriel22@gmail.com>
2024-10-08 09:53:27 -0300

    refactor: adding dynamic messages

--- app.js
+++ app.js
@@ -3,20 +3,33 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title><head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title><head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
     return res.end();
   }
 
   if (url === "/message" && method === "POST") {
-    console.log("OK");
-    fs.writeFileSync("message.txt", "test");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+    fs.writeFile("message.txt", message, err => {
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
+
+    });
+
   }
 });
 

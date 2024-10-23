002e7dd6dc27c7868cad0781d8545ca0b35c038b
hermes gabriel <programadorgabriel22@gmail.com>
2024-10-23 09:49:12 -0300

    feat: adding gitignore

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const http = require("http");
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("middleware here");
-  next();
+app.use("/add-product", (req, res, next) => {
+  res.send("<h1>test product</>");
 });
 
-const server = http.createServer(app);
-server.listen(1910);
+app.use("/", (req, res, next) => {
+  res.send("<h1>test homepage</>");
+});
+
+app.listen(1910);

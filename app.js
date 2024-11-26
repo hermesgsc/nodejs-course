e6260ad7f84defa6a799d8523688041f1cb0dbfa
hermesgsc <programadorgabriel22@gmail.com>
2024-11-26 08:52:05 -0300

    feat: add a 404 page

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const bodyParser = require("body-parser");
 const app = express();
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const path = require("path");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>ERROR 404</h1><p>This page doesn't exist</p>");
-  next();
+  res.status(404).sendFile(path.join(__dirname, "/views", "404.html"));
 });
 
-app.listen(1910, () => {
-  console.log("PORT: 1910");
+app.listen(3001, () => {
+  console.log("OK");
 });
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const path = require('path')
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">send</input></form>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"))
 });
 
 router.post("/product", (req, res, next) => {
-  console.log(req.body);
+  console.log(res.body);
   res.redirect("/");
 });
 
